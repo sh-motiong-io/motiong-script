b78c2bb3cd302dd43ef0dd880aa4f618a9e12316
yuansai <yuansai@coscene.io>
2023-10-09 18:25:18 +0800

    remove print

--- hello_world.py
+++ hello_world.py
@@ -12,7 +12,6 @@ def hello_world(count, name):
     click.echo("Hello world!")
 
 
-# By adding the following, this command can be executed
 # as a standalone CLI application
 if __name__ == "__main__":
     hello_world()
