b6355de1dc06aaf1de4dae1ea3896a43e0be9127
seahi <seahi@seahideMacBook-Pro.local>
2023-10-11 15:04:11 +0800

    find all page of a website and print the screenshot

--- cli.py
+++ cli.py
@@ -10,7 +10,7 @@ def entry_point():
     pass
 
 
-entry_point.add_command(spiders.printscreen)
+entry_point.add_command(spiders.save_website_screenshots, 'save-website-screenshots')
 entry_point.add_command(group2.foo)
 entry_point.add_command(hello_world)
 
--- spiders/commands.py
+++ spiders/commands.py
@@ -1,19 +1,21 @@
+import os
+from urllib.parse import urljoin, urlparse
+import uuid
 import click
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
-@click.command()
-@click.option('--url', default="https://www.motiong.com/", help='Enter the URL you want.')
-def printscreen(url):
-    # instance of Options class allows
-    # us to configure Headless Chrome
-    options = Options()
+# instance of Options class allows
+# us to configure Headless Chrome
+options = Options()
 
-    # it should be run without UI (Headless)
-    options.add_argument('--headless=new')
-    # initializing webdriver for Chrome with our options
-    driver = webdriver.Chrome(options=options)
+# it should be run without UI (Headless)
+options.add_argument('--headless=new')
+# initializing webdriver for Chrome with our options
+driver = webdriver.Chrome(options=options)
 
+def printscreen(url, filename):
     try:
         driver.get(url)
         print("Page URL:", driver.current_url)
@@ -22,14 +24,55 @@ def printscreen(url):
         # set the window size
         pageWidth = driver.execute_script("return document.body.parentNode.scrollWidth")
         pageHeight = driver.execute_script("return document.body.parentNode.scrollHeight")
-        driver.set_window_size(pageWidth, pageHeight + 150)
+        driver.set_window_size(pageWidth, pageHeight + 140)
 
         # save the screenshot
         # todo: save to a specific folder
-        driver.save_screenshot("capture.png")
+        driver.save_screenshot(filename)
 
     except Exception as e:
         print("Exception occurred " + repr(e))
 
-    finally:
-        driver.close()
+def get_all_links(url, paths = None):
+    if paths is None:
+        paths = set([url])
+    
+    # get all links by tag name
+    driver.get(url)
+    links = driver.find_elements(By.TAG_NAME, 'a')
+    hrefs = list(map(lambda link: link.get_attribute('href'), links))
+
+    for href in hrefs:
+        # transform relative url to absolute url
+        absolute_url = urljoin(url, href)
+
+        # check if the url is in the same domain
+        # check if the url is not already in the list
+        if absolute_url.startswith(url) and absolute_url not in paths:
+            paths.add(absolute_url)
+            get_all_links(absolute_url, paths)
+    return paths
+
+@click.command()
+@click.option('--url', default="https://www.motiong.com/", help='Enter the URL you want.')
+def save_website_screenshots(url):
+    links = get_all_links(url)
+
+    # create a folder for the domain
+    parsed_uri = urlparse(url)
+    domain = '{uri.netloc}'.format(uri=parsed_uri)
+    if os.path.exists(domain):
+       domain = domain + "-" + str(uuid.uuid1())
+    os.mkdir(domain)
+
+    folder_path = os.path.join(os.getcwd(), domain)
+    
+    for link in links:
+        # create filename from path
+        parsed_uri = urlparse(link)
+        path = '{uri.path}'.format(uri=parsed_uri)
+        filename = folder_path + "/" + path + ".png"
+
+        printscreen(link, filename)
+    
+    driver.close()
